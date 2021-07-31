16e94d4f2f2848aed6a72bda3e34a03c207d1c02
Roy Janechuti <roysahn@hotmail.com>
2021-07-30 21:55:03 -0700

    cleaned up the JavaScript code

--- calculator.js
+++ calculator.js
@@ -1,30 +1,5 @@
-let inputA = document.querySelector('#inputA');
-inputA.addEventListener('click', activateA);
-let spA = document.querySelector('#spA');
-
-let inputB = document.querySelector('#inputB');
-inputB.addEventListener('click', activateB);
-let spB = document.querySelector('#spB');
-
-let nb = document.querySelector('#numberButtons');
-nb.addEventListener('click', enterNumber);
-
-let ob = document.querySelector('#operationButtons');
-ob.addEventListener('click', enterOperand);
-let spO = document.querySelector('#spO');
-
-let cl = document.querySelector('#clear');
-cl.addEventListener('click', clearScreen);
-
-let bk = document.querySelector('#backspace');
-bk.addEventListener('click', backspace);
-
 let output = document.querySelector('#output');
 
-let h = document.querySelector('h1');
-h.addEventListener('click', function() {
-	welcome();
-});
 
 function main() {
 	let a = document.querySelector('main');
@@ -47,6 +22,8 @@ function backspace() {
 		activeSpan.innerText = newSpan.join('');
 	}
 }
+let bk = document.querySelector('#backspace');
+bk.addEventListener('click', backspace);
 
 function activateA() {
 	spA.classList.add('active');
@@ -54,6 +31,9 @@ function activateA() {
 	spB.classList.remove('active');
 	inputB.classList.remove('highlight');
 }
+let inputA = document.querySelector('#inputA');
+inputA.addEventListener('click', activateA);
+let spA = document.querySelector('#spA');
 
 function activateB() {
 	spB.classList.add('active');
@@ -61,6 +41,9 @@ function activateB() {
 	spA.classList.remove('active');
 	inputA.classList.remove('highlight');
 }
+let inputB = document.querySelector('#inputB');
+inputB.addEventListener('click', activateB);
+let spB = document.querySelector('#spB');
 
 function isActive() {
 	let result = document.querySelector('.active');
@@ -75,14 +58,62 @@ function enterNumber(e) {
 	activeSpan.innerText += text;
 	}
 }
+let nb = document.querySelector('#numberButtons');
+nb.addEventListener('click', enterNumber);
 
 function enterOperand(e) {
 	if(e.target.value == undefined) return;
 	spO.innerText = e.target.value;	
 }
+let ob = document.querySelector('#operationButtons');
+ob.addEventListener('click', enterOperand);
+let spO = document.querySelector('#spO');
 
-let equals = document.querySelector('#equals');
-equals.addEventListener('click', calculation);
+
+function clearScreen() {
+	let spanAll = document.querySelectorAll('span');
+	let spanLength = spanAll.length - 1;
+
+	for (x = 0; x <= spanLength; x++) {
+		spanAll[x].innerText = '';
+	}
+}
+let cl = document.querySelector('#clear');
+cl.addEventListener('click', clearScreen);
+
+function close(e) {
+	e.preventDefault();
+	removalDIV = this.parentNode;
+	parentUL = removalDIV.parentNode;
+	parentUL.removeChild(removalDIV);
+
+	main();
+	footer();
+	h.classList.remove('hide');
+}
+
+function welcome() {
+	let w = document.createElement('div');
+	let image = '<h3>Calculator by RHJ Design</h3>' +
+				'<img src="cartoon-calculator.png" alt="calculator cartoon" />' +
+				'<a href="#" class="exit">close X</a>';
+	w.innerHTML = image;
+	w.classList.add('modal');
+	document.body.appendChild(w);
+	let ex = document.querySelector('.exit');
+	ex.addEventListener('click', close);
+	h.classList.add('hide');
+
+	let a = document.querySelector('main');
+	a.classList.add('hide');
+
+	let f = document.querySelector('footer');
+	f.classList.add('hide');
+}
+let h = document.querySelector('h1');
+h.addEventListener('click', function() {
+	welcome();
+});
 
 function calculation() {
 	let a = spA.innerText;
@@ -115,44 +146,5 @@ function calculation() {
 		output.innerText = ' = ' + out;
 	}
 }
-
-function clearScreen() {
-	let spanAll = document.querySelectorAll('span');
-	let spanLength = spanAll.length - 1;
-
-	for (x = 0; x <= spanLength; x++) {
-		spanAll[x].innerText = '';
-	}
-}
-
-function welcome() {
-	let w = document.createElement('div');
-	let image = '<h3>Calculator by RHJ Design</h3>' +
-				'<img src="cartoon-calculator.png" alt="calculator cartoon" />' +
-				'<a href="#" class="exit">close X</a>';
-	w.innerHTML = image;
-	w.classList.add('modal');
-	document.body.appendChild(w);
-	let ex = document.querySelector('.exit');
-	ex.addEventListener('click', close);
-	h.classList.add('hide');
-
-	let a = document.querySelector('main');
-	a.classList.add('hide');
-
-	let f = document.querySelector('footer');
-	f.classList.add('hide');
-
-}
-
-function close(e) {
-	e.preventDefault();
-	removalDIV = this.parentNode;
-	parentUL = removalDIV.parentNode;
-	parentUL.removeChild(removalDIV);
-
-	main();
-	footer();
-	h.classList.remove('hide');
-
-}
+let equals = document.querySelector('#equals');
+equals.addEventListener('click', calculation);
